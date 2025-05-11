b0d2cc679fb37db048347b887b50060d544e7f7f
Saro jafari <109202685+Saro-jafari@users.noreply.github.com>
2025-05-11 10:39:26 +0330

    Add User Registration System with Improved Design

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SwingUtilities.invokeLater(() -> {
+            new LoginScreen(new LoginScreen.LoginCallback() {
+                @Override
+                public void onLoginSuccess() {
+                    SwingUtilities.invokeLater(() -> new TodoApp().setVisible(true));
+                }
+            }).setVisible(true);
+        });
     }
 }
